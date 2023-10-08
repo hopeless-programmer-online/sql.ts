320b85a6cde793b2462f7799cf605bd198fe0284
hopeless programmer <me@hopeless-programmer.online>
2023-10-08 20:30:45 +0300

    Add table

--- src/sql.test.ts
+++ src/sql.test.ts
@@ -1,11 +1,13 @@
 import * as sql from './sql'
 
-test(``, () => {
+test(`Empty database`, () => {
     const db = sql.database(`MY_DATABASE`)
 
     // static checks
     const database_name : `MY_DATABASE` = db.name
+    const database_tables : {} = db.tables
 
     expect(db[sql.type]).toBe(sql.Database[sql.type])
-    expect(db.name).toBe(`MY_DATABASE`)
+    expect(database_name).toBe(`MY_DATABASE`)
+    expect(database_tables).toMatchObject({})
 })
--- src/sql.ts
+++ src/sql.ts
@@ -2,19 +2,28 @@ export const type = Symbol(`sql.type`)
 
 export type ITable = Table<string>
 
+export type ITables = {
+    [name : string] : ITable
+}
+
 export class Database<
     Name extends string,
+    Tables extends ITables,
 > {
     public static readonly [type] : unique symbol = Symbol(`sql.Database`)
 
     public readonly name : Name
+    public readonly tables : Tables
 
     public constructor({
         name,
+        tables,
     } : {
         name : Name
+        tables : Tables
     }) {
         this.name = name
+        this.tables = tables
     }
 
     public get [type]() : typeof Database[typeof type] {
@@ -46,7 +55,8 @@ export class Table<
  * Creates an empty database.
  */
 export function database<Name extends string>(name : Name) {
-    const database = new Database({ name })
+    const tables = {} as const
+    const database = new Database({ name, tables })
 
     return database
 }
