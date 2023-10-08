f082f43f00713d8f36601823250bfa26faeab166
hopeless programmer <me@hopeless-programmer.online>
2023-10-08 20:21:28 +0300

    Update sql.ts

--- src/sql.ts
+++ src/sql.ts
@@ -1,5 +1,7 @@
 export const type = Symbol(`sql.type`)
 
+export type ITable = Table<string>
+
 export class Database<
     Name extends string,
 > {
@@ -20,6 +22,29 @@ export class Database<
     }
 }
 
+export class Table<
+    Name extends string,
+> {
+    public static readonly [type] : unique symbol = Symbol(`sql.Table`)
+
+    public readonly name : Name
+
+    public constructor({
+        name,
+    } : {
+        name : Name
+    }) {
+        this.name = name
+    }
+
+    public get [type]() : typeof Table[typeof type] {
+        return Table[type]
+    }
+}
+
+/**
+ * Creates an empty database.
+ */
 export function database<Name extends string>(name : Name) {
     const database = new Database({ name })
 
